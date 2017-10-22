e12d3dd878875518436cd15a13eb51dd7561b353
Gareth Redfern <gareth@garethredfern.com>
2017-10-22 09:02:10 +0100

    change the url for the sign-in page

--- src/main.js
+++ src/main.js
@@ -38,7 +38,7 @@ router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
 
   if (requiresAuth && !currentUser) {
-    next('/login');
+    next('/sign-in');
   } else if (requiresAuth && currentUser) {
     next();
   } else {
