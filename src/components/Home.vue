0251bc55e7ee9205766e0be426f496c95d622f72
Gareth Redfern <gareth@garethredfern.com>
2017-11-09 20:36:59 +0000

    Update home info text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <section class="section">
     <h1 class="title has-text-centered">Home</h1>
     <article class="notification is-primary">
-      <p>This is the home page for the app, it is <strong>not secured</strong>, and provides the page a user sees when they visit the site.</p>
+      <p>This is the home page for the app, it is <strong>not secured</strong>.</p>
     </article>
   </section>
 </template>
