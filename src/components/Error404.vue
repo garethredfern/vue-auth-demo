2bead3934d6bff121a17c1dd51bdf2f9695c19b1
Gareth Redfern <gareth@garethredfern.com>
2020-09-03 13:41:59 +0100

    add error page styles

--- src/components/Error404.vue
+++ src/components/Error404.vue
@@ -1,8 +1,10 @@
 <template>
   <section>
-    <h1 class="text-center">404 Page Missing</h1>
+    <h1 class="text-center mb-4 font-bold text-lg">404 Page Missing</h1>
     <article>
-      <p>This page displays when a route is accessed that doesn't exist.</p>
+      <p class="bg-red-100 p-5 border border-red-200 rounded">
+        This page displays when a route doesn't exist.
+      </p>
     </article>
   </section>
 </template>
