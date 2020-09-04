801f36f0b434dc96a4f366cc83858f313a30fad6
Gareth Redfern <gareth@garethredfern.com>
2020-09-04 10:18:08 +0100

    update text colour on alert message

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,7 +2,7 @@
   <section>
     <h1 class="text-center mb-4 font-bold text-lg">Dashboard</h1>
     <article>
-      <p class="bg-blue-100 p-5 border border-blue-200 rounded">
+      <p class="bg-blue-100 p-5 border border-blue-200 rounded text-blue-500">
         This is the dashboard area which is secured via the router. A user must
         have an account and be logged in to view this page.
       </p>
--- src/components/Error404.vue
+++ src/components/Error404.vue
@@ -2,7 +2,7 @@
   <section>
     <h1 class="text-center mb-4 font-bold text-lg">404 Page Missing</h1>
     <article>
-      <p class="bg-red-100 p-5 border border-red-200 rounded">
+      <p class="bg-red-100 p-5 border border-red-200 rounded text-red-500">
         This page displays when a route doesn't exist.
       </p>
     </article>
